be58b47546e823e8599d3494a37ba12a9533a5fd
Vinicius-Passos <vinicius.passos@kabum.com.br>
2022-08-05 17:55:58 -0300

    first commit

---
+++ layout/rodape.php
@@ -0,0 +1,10 @@
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="../web/js/wow.min.js"></script>
+    <script>
+        new WOW().init();
+    </script>
+    <script src="../web/js/home.js"></script>
+    <script src="../web/js/everest.js"></script>
+</body>
+</html>
---
+++ view/home.php
@@ -0,0 +1,136 @@
+<?php
+    require_once("../layout/cabecalho.php");
+?>
+
+
+<div class="small-line-top">
+</div>
+<!-- NAVBAR -->
+<nav class="navbar navbar-expand-lg bg-transparent">
+  <div class="container-fluid">
+    <a class="navbar-brand ms-5" href="#">Everest</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      </ul>
+      <div class="d-flex">
+        <ul class="navbar-nav me-5 mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+                <button type="button" class="button-nav-everest">
+                    Começar
+                </button>
+            </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+<!-- NAVBAR -->
+
+<!-- FIRST CONTAINER -->
+<div class="container apresentation-container">
+    <img id="shadowWave" class="wow bounceInUp" src="https://static.wixstatic.com/media/c837a6_f3dbcb551a5c4e9a831a89a0d362afcc~mv2.png/v1/fill/w_1494,h_742,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/c837a6_f3dbcb551a5c4e9a831a89a0d362afcc~mv2.png" alt="">
+
+    <div class="row wow bounceInUp mb-5">
+        <div class="col-md-3">
+            <div class="col-md-12 mb-5" id="textApresentation">
+                <h1 class="mb-5">Manage. Live. Enjoy</h1>
+                Enjoy, we care of your time
+            </div>
+            <div class="col-md-12 mb-5">
+                <button class="button-primary-everest">Instalar</button>
+            </div>
+            <i class="fas fa-clock"></i> Organize seu tempo
+            <br>
+            <i class="fas fa-fingerprint"></i> Segurança com as tarefas
+            <br>
+            <i class="fas fa-mobile-alt"></i> Acesse de onde quiser
+        </div>
+        <div class="col-md-3"></div>
+        <div class="col-md-6 wow bounceInUp d-flex justify-content-center align-center mb-5">
+            <img src="../web/images/undraw_reminder_u6i7.svg" width="400px" alt="Manager You Time">
+        </div>
+    </div>
+</div>
+<!-- FIRST CONTAINER -->
+
+<!-- FIRST TRACK -->
+<div class="first-track">
+    <div class="container">
+        <div class="row wow bounceInUp">
+            <div class="col-md-4">
+                <h1>Our solutions manage your time anytime, anywhere</h1>
+            </div>
+            <div class="col-md-3"></div>
+            <div class="col-md-5 wow bounceInUp text-justify">
+                Sou um parágrafo. Aqui você pode adicionar seu próprio texto. 
+                É fácil, é só clicar em "Editar texto" ou clicar duas vezes sobre mim para editar seu conteúdo e alterar a fonte. 
+                Você também pode me arrastar e soltar em qualquer lugar da página. Sou um ótimo espaço para você compartilhar a sua história.
+            </div>
+        </div>
+    </div>
+</div>
+<!-- FIRST TRACK -->
+
+<!-- SECOND CONTAINER -->
+<div class="second-container">
+    <div class="container">
+        <div class="row mt-5 row-header-cards">
+            <div class="col-md-3">
+                <h1>What we offer?</h1>
+                <p class="text-justify">Sou um parágrafo. Aqui você pode adicionar seu texto. É fácil, basta clicar em "Editar texto" ou clicar duas vezes sobre mim para editar o conteúdo, a fonte e mais.</p>
+            </div>
+        </div>
+        <div class="row row-cards">
+            <div class="col-md-3">
+                <div class="card border-dark mb-3" style="max-width: 18rem;">
+                    <img src="../web/images/undraw_team_collaboration_re_ow29.svg" width="100%" class="card-img-top">
+                </div>
+                <h3 class="mt-4">Team Management</h3>
+                <p class="mt-2">
+                    Solutions for the team manage and biggest projects.
+                </p>
+            </div>
+            <div class="col-md-3">
+                <div class="card border-dark mb-3" style="max-width: 18rem;">
+                    <img src="../web/images/undraw_events_re_98ue.svg" width="100%" class="card-img-top">
+                </div>
+                <h3 class="mt-4">Tasks Management</h3>
+                <p class="mt-2">
+                    Solution for time management, task management and colaboration with other peoples.
+                </p>
+            </div>
+            <div class="col-md-3">
+                <div class="card border-dark mb-3" style="max-width: 18rem;">
+                    <img src="../web/images/undraw_security_re_a2rk.svg" width="100%" class="card-img-top">
+                </div>
+                <h3 class="mt-4">Security</h3>
+                <p class="mt-2">
+                    Solution with high security, your security is our highest priority.
+                </p>
+            </div>
+            <div class="col-md-3">
+                <div class="card border-dark mb-3" style="max-width: 18rem;">
+                    <img src="../web/images/undraw_cloud_sync_re_02p1.svg" width="100%" class="card-img-top">
+                </div>
+                <h3 class="mt-4">Workflow</h3>
+                <p class="mt-2">
+                    Solution for time management, task management and colaboration with other peoples.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- SECOND CONTAINER -->
+
+<?php
+    require_once("../layout/rodape.php");
+?>
