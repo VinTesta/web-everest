6e1fcd6245c1753532e3420952ba804b279c6dd9
VinTesta <70596205+VinTesta@users.noreply.github.com>
2022-08-06 14:22:20 -0300

    feat: correção da responsividade

--- view/home.php
+++ view/home.php
@@ -39,14 +39,14 @@
 <div class="container apresentation-container">
     <img id="shadowWave" class="wow bounceInUp" src="https://static.wixstatic.com/media/c837a6_f3dbcb551a5c4e9a831a89a0d362afcc~mv2.png/v1/fill/w_1494,h_742,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/c837a6_f3dbcb551a5c4e9a831a89a0d362afcc~mv2.png" alt="">
 
-    <div class="row wow bounceInUp mb-5">
-        <div class="col-md-3">
-            <div class="col-md-12 mb-5" id="textApresentation">
-                <h1 class="mb-5">Manage. Live. Enjoy</h1>
+    <div class="row mb-5">
+        <div class="col-md-3 text-center mb-4 mt-5">
+            <div class="col-12 mb-5 text-center" id="textApresentation">
+                <h1 class="mb-5 text-center">Manage. Live. Enjoy</h1>
                 Enjoy, we care of your time
             </div>
             <div class="col-md-12 mb-5">
-                <button class="button-primary-everest">Instalar</button>
+                <button class="button-primary-everest ms-4">Instalar</button>
             </div>
             <i class="fas fa-clock"></i> Organize seu tempo
             <br>
@@ -54,9 +54,9 @@
             <br>
             <i class="fas fa-mobile-alt"></i> Acesse de onde quiser
         </div>
-        <div class="col-md-3"></div>
-        <div class="col-md-6 wow bounceInUp d-flex justify-content-center align-center mb-5">
-            <img src="../web/images/undraw_reminder_u6i7.svg" width="400px" alt="Manager You Time">
+        <div class="col-md-3 mb-4"></div>
+        <div class="col-md-6 col-12 d-flex justify-content-center align-center mb-5" id="divImgFirst">
+            <img src="../web/images/undraw_reminder_u6i7.svg" id="img-first-container" alt="Manager You Time">
         </div>
     </div>
 </div>
@@ -64,10 +64,10 @@
 
 <!-- FIRST TRACK -->
 <div class="first-track">
-    <div class="container">
+    <div class="container-xs">
         <div class="row wow bounceInUp">
             <div class="col-md-4">
-                <h1>Our solutions manage your time anytime, anywhere</h1>
+                <h1>Why Everest solutions?</h1>
             </div>
             <div class="col-md-3"></div>
             <div class="col-md-5 wow bounceInUp text-justify">
@@ -82,7 +82,7 @@
 
 <!-- SECOND CONTAINER -->
 <div class="second-container">
-    <div class="container">
+    <div class="container-md">
         <div class="row mt-5 row-header-cards">
             <div class="col-md-3">
                 <h1>What we offer?</h1>
@@ -90,7 +90,7 @@
             </div>
         </div>
         <div class="row row-cards">
-            <div class="col-md-3">
+            <div class="col-md-3 mb-5">
                 <div class="card border-dark mb-3" style="max-width: 18rem;">
                     <img src="../web/images/undraw_team_collaboration_re_ow29.svg" width="100%" class="card-img-top">
                 </div>
@@ -99,7 +99,7 @@
                     Solutions for the team manage and biggest projects.
                 </p>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-3 mb-5">
                 <div class="card border-dark mb-3" style="max-width: 18rem;">
                     <img src="../web/images/undraw_events_re_98ue.svg" width="100%" class="card-img-top">
                 </div>
@@ -108,7 +108,7 @@
                     Solution for time management, task management and colaboration with other peoples.
                 </p>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-3 mb-5">
                 <div class="card border-dark mb-3" style="max-width: 18rem;">
                     <img src="../web/images/undraw_security_re_a2rk.svg" width="100%" class="card-img-top">
                 </div>
@@ -117,7 +117,7 @@
                     Solution with high security, your security is our highest priority.
                 </p>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-3 mb-5">
                 <div class="card border-dark mb-3" style="max-width: 18rem;">
                     <img src="../web/images/undraw_cloud_sync_re_02p1.svg" width="100%" class="card-img-top">
                 </div>
@@ -130,6 +130,24 @@
     </div>
 </div>
 <!-- SECOND CONTAINER -->
+
+<!-- CONTAINER INSTALATION -->
+<div class="second-track">
+    <div class="container-xs">
+        <div class="row wow bounceInUp">
+            <div class="col-md-5 wow bounceInUp text-justify">
+                Sou um parágrafo. Aqui você pode adicionar seu próprio texto. 
+                É fácil, é só clicar em "Editar texto" ou clicar duas vezes sobre mim para editar seu conteúdo e alterar a fonte. 
+                Você também pode me arrastar e soltar em qualquer lugar da página. Sou um ótimo espaço para você compartilhar a sua história.
+            </div>
+            <div class="col-md-3"></div>
+            <div class="col-md-4">
+                <h1>Our solutions manage your time anytime, anywhere</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- CONTAINER INSTALATION -->
 
 <?php
     require_once("../layout/rodape.php");
