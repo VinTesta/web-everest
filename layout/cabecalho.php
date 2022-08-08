849d495b4a79f8f312277aa3e7dc3af33390d628
Vinicius-Passos <vinicius.passos@kabum.com.br>
2022-08-08 16:25:36 -0300

    feat: adicionado about us

--- layout/cabecalho.php
+++ layout/cabecalho.php
@@ -9,11 +9,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="../web/css/everest.css">
     <link rel="stylesheet" href="../web/css/home.css">
-    <script src="https://kit.fontawesome.com/072fea83f9.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../web/css/about-us.css">
+    <script src="https://kit.fontawesome.com/5b48296146.js" crossorigin="anonymous"></script>
     <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
     />
+
+    <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
 </head>
 <body class="my-bg-main">
     
@@ -23,7 +26,7 @@
     <!-- NAVBAR -->
     <nav class="navbar navbar-expand-lg bg-transparent">
     <div class="container-fluid">
-        <a class="navbar-brand ms-5" href="#">Everest</a>
+        <a class="navbar-brand ms-5" href="../">Everest</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
@@ -33,10 +36,10 @@
         <div class="d-flex">
             <ul class="navbar-nav me-5 mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="../">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">About Us</a>
+                    <a class="nav-link" href="./about-us.php">About Us</a>
                 </li>
                 <li class="nav-item">
                     <button type="button" class="button-nav-everest">
---
+++ view/about-us.php
@@ -0,0 +1,83 @@
+<?php
+    require_once("../layout/cabecalho.php");
+?>
+
+<!-- FIRST CONTAINER -->
+<div class="container about-us-container">
+    <div class="row mb-5">
+        <h1 class="mb-5">About Us</h1>
+        <div class="col-12">
+            <p>O Everest foi criado através de uma ideia, uma necessidade e um sonho; tonar a agenda pessoal algo intuitivo, trazer funcionalidades
+             que cada vez mais ajudam o mercado de trabalho e se tornar a agenda mais amada pelos nossos usuários.</p>
+            <p>Essas palavras combinadas a muito café e determinação foram os ingredientes para contruir o <b>Everest</b>.</p>
+        </div>                
+    </div>
+    <div class="row" style="margin-bottom: 100px">
+        <h1>The backstage minds</h1>
+        <div class="col-md-12">
+            <p>
+                É claro que não foi apenas café e uma ideia boa que criaram o Everest. Para tudo se realizar foram necessárias duas mentes brilhantes
+                empenhadas em dar o seu melhor por isso.
+            </p>
+            <p>
+                Dois estudantes do Colégio Técnico de Limeira - UNICAMP (COTIL) foram os idealizadores do projeto.
+            </p>
+        </div>
+    </div>
+    <div class="row" style="margin-bottom: 100px">
+        <div class="col-md-4 mb-4">
+            <div class="badge-base LI-profile-badge" data-locale="pt_BR" data-size="large" data-theme="dark" data-type="HORIZONTAL" data-vanity="vinicius-testa-passos" data-version="v1">
+                <a class="badge-base__link LI-simple-link" href="https://br.linkedin.com/in/vinicius-testa-passos?trk=profile-badge">
+                </a>
+            </div>
+        </div>
+        <div class="col-md-6 mb-5">
+            <h3>Vinicius Testa Passos</h3>
+            <p>
+                Aspirante a programador desde criança Jheny (Jheniffer Paola de Faria) foi apaixonado por técnologia e suas vertentes. Atualmente ocupa
+                um dos cargos de Analista de Desenvolvimento FullStack no KaBuM! e diz levar o trabalho como um hobby e não uma obrigação.
+            </p>
+            <div class="icons-social-medias">
+                <a href="#">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>
+                <a href="#">
+                    <i class="fa-solid fa-at"></i>
+                </a>
+                <a href="#">
+                    <i class="fa-brands fa-github"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6 mb-4 text-end">
+            <h3>Jheniffer Paola de Faria</h3>
+            <p>
+                Aspirante a programador desde criança Testa (Vinicius Testa Passos) foi apaixonado por técnologia e suas vertentes. Atualmente ocupa
+                um dos cargos de Analista de Desenvolvimento FullStack no KaBuM! e diz levar o trabalho como um hobby e não uma obrigação.
+            </p>
+            <div class="icons-social-medias">
+                <a href="#">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>
+                <a href="#">
+                    <i class="fa-solid fa-at"></i>
+                </a>
+                <a href="#">
+                    <i class="fa-brands fa-github"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-3 mb-5 d-flex align-end">
+            <div class="badge-base LI-profile-badge" data-locale="pt_BR" data-size="large" data-theme="dark" data-type="HORIZONTAL" data-vanity="jheniffer-faria-0067401bb" data-version="v1">
+                <a class="badge-base__link LI-simple-link" href="https://br.linkedin.com/in/jheniffer-faria-0067401bb?trk=profile-badge">
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+    require_once("../layout/rodape.php");
+?>
